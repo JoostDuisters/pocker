6f8cd0ca31a2685df27e735ec264d14d11b0b9f9
Joost Duisters <j.h.duisters@gmail.com>
2020-06-06 17:13:27 +0100

    Create pocker pull command with unshare and mount

--- pocker.py
+++ pocker.py
@@ -3,16 +3,16 @@ import shutil
 import os
 import uuid
 import tarfile
+import subprocess
+import syscalls
 
 
-def docker_pull(image, tag):
+def pocker_pull(image, tag, image_hash, root_dir):
     # Hardcode these for now. Should work in EC2
     architecture = "amd64"
     os_system = "linux"
 
     # Directory where image will end up
-    root_dir = './testing' # TODO: make this parameterizable
-    image_hash = uuid.uuid4().hex
     image_dir = root_dir + "/" + image_hash
 
     # Initiate docker registry object and pull first manifest
@@ -48,5 +48,37 @@ def docker_pull(image, tag):
             return None
 
     process_manifest(manifest, registry)
-    return image_dir
+
+
+def pocker_run(image, tag, cmd=["/bin/bash"]):
+
+    # Directory where image will end up
+    root_dir = './testing'  # TODO: make this parameterizable
+    image_hash = uuid.uuid4().hex
+    image_dir = root_dir + "/" + image_hash
+
+    # TODO: make it keep track of images that have already been pulled.
+    pocker_pull(image, tag, image_hash, root_dir)
+
+    # unshare
+    syscalls.unshare(
+        syscalls.CLONE_NEWUTS |
+        syscalls.CLONE_NEWPID |
+        syscalls.CLONE_NEWNET |
+        syscalls.CLONE_NEWNS)  # uts, pid, mount, ipc
+
+    # change root to image dir
+    os.chroot(image_dir)
+    os.chdir('/')
+    syscalls.mount('/proc', '/proc', 'proc')
+
+    # Execute command
+    subprocess.run(cmd)
+
+
+pocker_run("alpine", "latest")
+
+
+
+
 
---
+++ syscalls.py
@@ -0,0 +1,35 @@
+import os
+import ctypes
+
+
+libc = ctypes.CDLL(None)
+
+get_errno_loc = libc.__errno_location
+get_errno_loc.restype = ctypes.POINTER(ctypes.c_int)
+
+# Constants from https://github.com/karelzak/util-linux/blob/master/include/namespace.h
+CLONE_NEWNS = 0x00020000
+CLONE_NEWCGROUP = 0x02000000
+CLONE_NEWUTS = 0x04000000
+CLONE_NEWIPC = 0x08000000
+CLONE_NEWNET = 0x40000000
+CLONE_NEWUSER = 0x10000000
+CLONE_NEWPID = 0x20000000
+CLONE_NEWTIME = 0x00000080
+
+
+def mount(source, target, fs):
+    rc = libc.mount(source.encode(), target.encode(), fs.encode(), 0)
+    if rc == -1:
+        raise Exception(os.strerror(get_errno_loc()[0]))
+
+
+def unshare(flags):
+    rc = libc.unshare(flags)
+    if rc == -1:
+        raise Exception(os.strerror(get_errno_loc()[0]))
+
+
+
+
+
